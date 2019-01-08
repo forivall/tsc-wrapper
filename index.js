be05bb639cc82c05ea29b027ccace7925b10641b
Tyler Waters <tyler.waters@gmail.com>
2019-01-07 20:32:42 -0800

    async/await-ify; bump major

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
 
 'use strict'
 
+const {promisify} = require('util')
 const fs = require('fs')
 const ts = require('typescript')
 
-module.exports = (configFileName, isProd) => {
-  let configFileText = null
-  try {
-    configFileText = fs.readFileSync(configFileName).toString()
-  }
-  catch (e) {
-    return Promise.reject(e)
-  }
+const readFile = promisify(fs.readFile)
 
+
+module.exports = async configFileName => {
+  const configFileText = await readFile(configFileName, 'utf-8')
   const configResult = ts.parseConfigFileTextToJson(configFileName, configFileText)
   const configObject = configResult.config
 
-  if (!configObject) {
-    return Promise.reject(configResult.error)
+  if (configObject == null) {
+    throw configResult.error
   }
 
-  const configParseResult = ts.parseJsonConfigFileContent(configObject, ts.sys, ts.getDirectoryPath(configFileName), null, configFileName)
-  if (isProd) {
-    configParseResult.options.sourceMap = false
-    configParseResult.options.inlineSourceMap = false
-    configParseResult.options.sourceRoot = null
-    configParseResult.options.removeComments = true
-  }
-
-  const compilerOptions = configParseResult.options
-  const compilerHost = ts.createCompilerHost(compilerOptions)
-  const rootFileNames = configParseResult.fileNames
-  const program = ts.createProgram(rootFileNames, compilerOptions, compilerHost)
+  const {fileNames, options} = ts.parseJsonConfigFileContent(configObject, ts.sys, ts.getDirectoryPath(configFileName), null, configFileName)
+  const compilerHost = ts.createCompilerHost(options)
+  const program = ts.createProgram(fileNames, options, compilerHost)
   const result = program.emit()
 
   const diagnostics = ts.getPreEmitDiagnostics(program).concat(result.diagnostics)
-  if (diagnostics.length === 0) {
-    return Promise.resolve()
+  if (diagnostics.length > 0) {
+    const errors = diagnostics.map(({file, start, messageText}) => {
+      const {line, character} = file.getLineAndCharacterOfPosition(start)
+      const message = ts.flattenDiagnosticMessageText(messageText, '\n')
+      return `${file.fileName} (${line + 1}, ${character + 1}): ${message}`
+    })
+    throw errors
   }
-
-  return Promise.reject(diagnostics.map((diagnostic) => {
-    const {line, character} = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start)
-    const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')
-    return `${diagnostic.file.fileName} (${line + 1}, ${character + 1}): ${message}`
-  }))
 
 }
--- test/compile.test.js
+++ test/compile.test.js
@@ -2,72 +2,76 @@
 'use strict'
 
 const assert = require('assert')
-const tscWrapper = require('..')
+const {promisify} = require('util')
 const fs = require('fs.extra')
+
+const tscWrapper = require('..')
+
+const mkdir = promisify(fs.mkdir)
+const rmrf = promisify(fs.rmrf)
+const readFile = promisify(fs.readFile)
+const stat = promisify(fs.stat)
 
 describe('compile', () => {
 
-  beforeEach((cb) => {
-    fs.mkdir('./test/out', cb)
+  beforeEach(() => mkdir('./test/out'))
+
+  afterEach(() => rmrf('./test/out'))
+
+  it('should reject if the config file doesnt exist', async () => {
+    let result = null
+    let error = null
+
+    try {
+      result = await tscWrapper('./test/fixtures/noooope.json')
+    } catch (err) {
+      error = err
+    }
+
+    assert.equal(result, null)
+    assert.equal(error.code, 'ENOENT')
   })
 
-  afterEach((cb) => {
-    fs.rmrf('./test/out', cb)
+  it('should reject file problems with config', async () => {
+    let result = null
+    let error = null
+
+    try {
+      result = await tscWrapper('./test/fixtures/tsconfig-bad-json.json')
+    } catch (err) {
+      error = err
+    }
+
+    assert.equal(result, null)
+    assert.equal(error.messageText, 'Failed to parse file \'./test/fixtures/tsconfig-bad-json.json\': Unexpected end of JSON input.')
   })
 
-  it('should reject if the config file doesnt exist', () => {
-    return tscWrapper('./test/fixtures/noooope.json')
-      .then(() => { throw new Error('expected resolution') })
-      .catch((e) => { assert.equal(e.code, 'ENOENT') })
-  })
-
-  it('should reject file problems with config', () => {
-    return tscWrapper('./test/fixtures/tsconfig-bad-json.json')
-      .then(() => { throw new Error('expected resolution') })
-      .catch((e) => { assert.equal(e.messageText, 'Failed to parse file \'./test/fixtures/tsconfig-bad-json.json\': Unexpected end of JSON input.') })
-  })
-
-  it('should reject with errors if tsc blows up', function () {
+  it('should reject with errors if tsc blows up', async function () {
+    let result = null
+    let error = null
     this.slow(2000)
-    return tscWrapper('./test/fixtures/tsconfig-invalid.json')
-      .then(() => { throw new Error('expected resolution') })
-      .catch((e) => { assert(e.length > 0) })
+
+    try {
+      result = await tscWrapper('./test/fixtures/tsconfig-invalid.json')
+    } catch (err) {
+      error = err
+    }
+
+    assert.equal(result, null)
+    assert(error.length > 0)
   })
 
-  it('should take the happy path quite nicely', function () {
+  it('should take the happy path quite nicely', async function () {
     this.slow(2000)
-    return tscWrapper('./test/fixtures/tsconfig-valid.json')
-      .then(() => {
-        const result = fs.readFileSync('./test/out/good.js')
-        assert(result.indexOf('comment') > -1)
-        assert(result.indexOf('sourceMappingURL') > -1)
-      })
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          fs.stat('./test/out/good.js.map', (e) => {
-            if (e) { reject(new Error('sourcemap not created')) }
-            else { resolve() }
-          })
-        })
-      })
-  })
 
-  it('should take the happy path quite nicely - prod', function () {
-    this.slow(2000)
-    return tscWrapper('./test/fixtures/tsconfig-valid.json', true)
-      .then(() => {
-        const result = fs.readFileSync('./test/out/good.js')
-        assert(result.indexOf('comment') === -1)
-        assert(result.indexOf('sourceMappingURL') === -1)
-      })
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          fs.stat('./test/out/good.js.map', (e) => {
-            if (!e) { reject(new Error('sourcemap created')) }
-            else { resolve() }
-          })
-        })
-      })
+    await tscWrapper('./test/fixtures/tsconfig-valid.json')
+
+    const result = await readFile('./test/out/good.js')
+    assert(result.indexOf('comment') > -1)
+    assert(result.indexOf('sourceMappingURL') > -1)
+
+    const sourcemap = await stat('./test/out/good.js.map')
+    assert(sourcemap)
   })
 
 })
